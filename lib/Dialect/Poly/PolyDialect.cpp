b8deb1a38c68df02f34786c8529de262841d841f
Tryorish <2575550173@qq.com>
2026-09-01 10:53:39 +0800

    fold

--- lib/Dialect/Poly/PolyDialect.cpp
+++ lib/Dialect/Poly/PolyDialect.cpp
@@ -25,6 +25,16 @@ void PolyDialect::initialize() {
     >();
 }
 
+Operation* PolyDialect::materializeConstant(OpBuilder &builder, Attribute value, Type type, Location loc)
+{
+    auto coeffs = dyn_cast<DenseIntElementsAttr>(value);
+    if (!coeffs) {
+        return nullptr;
+    }
+    return builder.create<ConstantOp>(loc, type, coeffs);
+}
+
+
 }
 }
 }
